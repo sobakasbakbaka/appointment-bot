26e0ec323d3dfdb351fff8621a120824638869c4
Nikita <108035712+sobakasbakbaka@users.noreply.github.com>
2024-08-08 01:03:52 +0300

    added field completed into schema

--- prisma/migrations/20240807220120_init/migration.sql
+++ prisma/migrations/20240807220120_init/migration.sql
@@ -1,6 +1,7 @@
 -- CreateTable
 CREATE TABLE "Appointment" (
     "id" SERIAL NOT NULL,
+    "completed" BOOLEAN NOT NULL DEFAULT false,
     "name" TEXT NOT NULL,
     "phone" TEXT NOT NULL,
     "time" TIMESTAMP(3) NOT NULL,
